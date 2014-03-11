247dd99a2091364a03c110ee0d55bfe913d4614f
Charlie Sun <sunworld23@gmail.com>
2014-03-11 18:30:45 -0400

    data now displayed in 2D array

--- StockTracker/src/ExcelTest.java
+++ StockTracker/src/ExcelTest.java
@@ -1,30 +1,65 @@
-import java.io.*;
 import java.sql.*;
 
 public class ExcelTest {
-	public static void main(String[] args) {
-		//Connection connection = null;
+	protected String [] rawdata;
+	protected int totalrows;
+	protected int totalcols;
+	protected String [][] stocklist;
+	
+	ExcelTest(){
 		try {
 			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 			Connection con = DriverManager
-					.getConnection("jdbc:odbc:qa-list");
-			Statement st = con.createStatement();
+					.getConnection("jdbc:odbc:COSC330");
+			// Allows you to scroll back
+			Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, 
+					ResultSet.CONCUR_READ_ONLY);
+			
 			ResultSet rs = st.executeQuery("Select * from [Sheet1$]");
 
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int numberOfColumns = rsmd.getColumnCount();
-
-			System.out.println("No of cols " + numberOfColumns);
-
-			while (rs.next()) {
-				for (int i = 1; i <= 1; i++) {
-					if (i > 1)
-						System.out.print(", ");
-					String columnValue = rs.getString(i);
-					System.out.print(columnValue);
-				}
-				System.out.println("");
+			int numberOfRows = 0;
+			
+			// Gets number of Rows
+			while (rs.next()){
+				  numberOfRows++;
 			}
+			
+			totalrows = numberOfRows - 1;
+			totalcols = numberOfColumns - 1;
+			
+			
+			// Tracker Delete later
+//			System.out.println("No of rows " + numberOfRows);
+//			System.out.println("No of cols " + numberOfColumns);
+			
+			
+			
+			rawdata = new String[numberOfRows];
+			int index = 0;
+
+			// Gets first row
+			rs.first();
+			rawdata[index] = rs.getString(1);
+			index++;
+			
+			while(rs.next()){
+				String columnValue = rs.getString(1);
+//				System.out.print(columnValue);
+//				System.out.println("");
+				
+				rawdata[index] = columnValue;
+				index++;
+			}
+			
+			System.out.println("");
+			
+//			for(int i = 0; i < numberOfRows - 1; i++){
+//				System.out.print(rawdata[i]);
+//				System.out.println("");
+//			}
+			
 			rs.close();
 			st.close();
 			con.close();
@@ -33,4 +68,30 @@ public class ExcelTest {
 			System.err.println(ex.getMessage());
 		}
 	}
+	
+	public static void main(String[] args) {
+		ExcelTest run = new ExcelTest();
+		run.sortArray();
+		
+	}
+	
+	public void sortArray()
+	{
+		stocklist = new String[totalrows][];
+		
+		for(int i = 0; i < totalrows; i++){
+			stocklist[i] = rawdata[i].split(",");
+		}		
+		
+		// Printed Array
+		for(int i = 0; i < totalrows; i++){
+			for(int j = 0; j < totalcols; j++){
+				stocklist[i][j] = stocklist[i][j].replace("\"","");
+				System.out.print(stocklist[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	
 }
